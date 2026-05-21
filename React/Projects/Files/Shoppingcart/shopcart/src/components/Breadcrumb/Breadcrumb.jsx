5e16befb357fe5ae30cb3614d16f79585d7766f0
parmis delfani <parmis.delfani84@gmail.com>
2026-05-21 18:06:08 +0330

    shopping pro

---
+++ React/Projects/Files/Shoppingcart/shopcart/src/components/Breadcrumb/Breadcrumb.jsx
@@ -0,0 +1,23 @@
+import "./Breadcrumb.css";
+
+function Breadcrumb() {
+  return (
+    <nav aria-label="breadcrumb" className="w-max">
+      <ol className="breadcrumb-list">
+        <li className="breadcrumb-item">
+          <a href="#">خانه</a>
+          <span className="seperator"> / </span>
+        </li>
+        <li className="breadcrumb-item">
+          <a href="#">دوره ها</a>
+          <span className="seperator"> / </span>
+        </li>
+        <li className="breadcrumb-item">
+          <a href="#">فرانت اند</a>
+        </li>
+      </ol>
+    </nav>
+  );
+}
+
+export default Breadcrumb;
