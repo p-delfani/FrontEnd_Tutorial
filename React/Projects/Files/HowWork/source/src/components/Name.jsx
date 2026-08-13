53bf57495ba67f2ded6e94629be99ea4b441d6c2
parmis delfani <parmis.delfani84@gmail.com>
2026-08-13 18:31:42 +0330

    Work

---
+++ React/Projects/Files/HowWork/source/src/components/Name.jsx
@@ -0,0 +1,24 @@
+import { memo, useState } from "react";
+
+const Name = ({ name, setName }) => {
+  const [theme, setTheme] = useState("light");
+  // Context - useContext 
+
+  console.log(`[Name.jsx] Re-Rendered`);
+
+  return (
+    <>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        className={theme === "light" ? "light-mode" : "dark-mode"}
+      />
+      <button onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
+        تغییر تم
+      </button>
+    </>
+  );
+};
+
+export default memo(Name); // Props
